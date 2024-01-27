2e6900f7e29776968b01b9c7c83dcc2883674aa9
Markus Frey <maggifrey@gmx.de>
2024-01-27 23:46:09 +0100

    refactor: Change port to 80 and use index.html

--- ws+http-server.js
+++ ws+http-server.js
@@ -6,6 +6,6 @@ const Broadcaster = require('./raspivid-broadcaster');
 const app = express();
 app.use(express.static(__dirname + '/site'));
 const httpServer = http.createServer(app);
-httpServer.listen(2050);
+httpServer.listen(80);
 
-new Broadcaster({width: 640, height: 480, framerate: 20, port: 8080});
+new Broadcaster({width: 1280, height: 1024, framerate: 24, port: 8080});

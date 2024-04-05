03c298c2d5390c03ced34f9ff6eda48181e9b0a9
Markus Frey <maggifrey@gmx.de>
2024-04-05 22:20:07 +0200

    feat: libcamera-vid

--- stream.sh
+++ stream.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-raspivid -t 0 -w 640 -h 480 -fps 30 -b 1000000 -o - | ffmpeg -re -framerate 30 -i - -f lavfi -i anullsrc -c:v copy -f flv rtmp://a.rtmp.youtube.com/live2/[TOKEN]
+TOKEN=
+libcamera-vid --inline --nopreview -t 0 --width 1280 --height 960 --framerate 10 --codec h264 -g 50 --awbgains 2.0,2.0 --saturation -100 --brightness 0.65 --contrast 2.25 --sharpness 1.0 --shutter 100000 -b 1000000 -o - | ffmpeg -re -framerate 10 -i - -f lavfi -i anullsrc -c:v copy -f flv "rtmp://a.rtmp.youtube.com/live2/$TOKEN"
